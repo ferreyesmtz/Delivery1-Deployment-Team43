6bce9b20bc0e16324ae755adbdd4766cdc7244ba
ferreyesmtz <167777596+ferreyesmtz@users.noreply.github.com>
2025-03-14 22:39:25 +0000

    Modificacion total del proyecto

--- App.java
+++ App.java
@@ -1,5 +1,11 @@
-public class App {
-    public static void app(String[] args) {
-        System.out.println("Hello World!");
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 }
